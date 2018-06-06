e4ea6198df53f6be98264c52116003a04eb6f019
Wambo43 <Wambo43@users.noreply.github.com>
2018-06-06 12:03:09 +0200

    add current Project

---
+++ ExampleAndroidSpeechRecognition/Assets/Scripts/AndroidSpeechReconizer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AndroidSpeechReconizer : MonoBehaviour {
+
+    AndroidJavaClass pluginClass;
+   
+    public Text uiText;
+
+    void Start()
+    {
+        pluginClass = new AndroidJavaClass(
+            "de.maibornwolff.speechrecognition.plugin.SpeechRecFragment");
+        pluginClass.CallStatic("start", gameObject.name);
+        pluginClass.CallStatic("promptSpeechInput");
+    }
+
+    public void OnSpeechRecognitionResult(string result)
+    {
+        uiText.text = result;
+    }
+}
